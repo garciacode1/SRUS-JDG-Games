ad251dfa2979c8016e99bd6673b0ab0fa7152b1e
Juan Garcia <juandavidmalta@gmail.com>
2026-05-10 23:14:24 +0800

    add: player test file - unittests

--- app/player.py
+++ app/player.py
@@ -1,8 +1,10 @@
 class Player:
+    """Class player and attributes"""
     def __init__(self, uid, name):
-        self.__uid = uid,
+        self.__uid = uid
         self.__name = name
 
+    """private instance variables"""
     @property
     def uid(self):
         return self.__uid
@@ -10,3 +12,7 @@ class Player:
     @property
     def name(self):
         return self.__name
+
+    """string method to return readable string ->player object"""
+    def __str__(self):
+        return f"Player: {self.__name}, Unique id: {self.__uid}"
