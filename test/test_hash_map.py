4378464d45c9740037cc8f10e90284603cb4dcdd
Juan Garcia <juandavidmalta@gmail.com>
2026-05-23 15:49:52 +0800

    Add unittest for get

--- test/test_hash_map.py
+++ test/test_hash_map.py
@@ -2,5 +2,15 @@ import unittest
 
 from app.player_hash_map import PlayerHashMap
 
+
 class TestHashMap(unittest.TestCase):
-    
+
+    def test_get_nonexistent_player(self):
+        player_hash_map = PlayerHashMap()
+
+        player = player_hash_map.get("p20")
+        self.assertIsNone(player)
+
+
+if __name__ == "__main__":
+    unittest.main()
