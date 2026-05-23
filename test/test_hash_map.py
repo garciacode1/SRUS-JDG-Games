1cb9074e711f0ef6b23761380d99657c3517651e
Juan Garcia <juandavidmalta@gmail.com>
2026-05-23 15:56:33 +0800

    Add unittest for put

--- test/test_hash_map.py
+++ test/test_hash_map.py
@@ -4,8 +4,20 @@ from app.player_hash_map import PlayerHashMap
 
 
 class TestHashMap(unittest.TestCase):
+    def test_if_put_updates_an_already_existing_player(self):
+
+        player_hash_map = PlayerHashMap()
+
+        player_hash_map.put("p1", "John")
+        player_hash_map.put("p1", "Peter")
+
+        player = player_hash_map.get("p1")
+
+        self.assertEqual("Peter", player.name)
+        self.assertEqual(1, player_hash_map.size())
 
     def test_get_nonexistent_player(self):
+
         player_hash_map = PlayerHashMap()
 
         player = player_hash_map.get("p20")
